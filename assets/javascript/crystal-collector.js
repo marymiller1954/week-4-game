5c54e54523f983a7ea3315136678715b8de354cc
Mary Miller <marymiller1954@gmail.com>
2016-08-11 11:13:25 -0400

    Initial Commit

---
+++ assets/javascript/crystal-collector.js
@@ -0,0 +1,97 @@
+var audioElement=document.createElement('audio');
+audioElement.setAttribute('src','assets/images/magicwand.wav');
+var audioLost=document.createElement('audio');
+audioLost.setAttribute('src','assets/images/sad trombone.wav');
+var audioWin=document.createElement('audio');
+audioWin.setAttribute('src','assets/images/applause.wav');
+var audiostart=document.createElement('audio');
+audiostart.setAttribute('src','assets/images/magicwand.wav');
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+	crystals = ['assets/images/crystal1.jpg',
+	'assets/images/crystal2.jpg',
+	'assets/images/crystal3.jpe',
+	'assets/images/crystal7.jpg' ]
+	var counter = 0;
+	var wins = 0;
+	var losses = 0;
+	$('#win').text(wins);
+	$('#loss').text(losses);
+	
+	newCrystals();
+	newGame();
+
+	function newCrystals () {
+		var numbers = []
+			while(numbers.length < 4){
+			  var randomnumber = Math.ceil(Math.random()*12)
+			  var found = false;
+			  for (var i=0; i< numbers.length; i++){
+				if (numbers[i] == randomnumber){
+					found = true; break
+				}
+			  }
+			  if(!found)numbers[numbers.length]=randomnumber;
+			}
+		console.log(numbers);		
+
+		for (i = 0; i < numbers.length; i++) {
+			var imageCrystal = $('<img>');
+			imageCrystal.attr('data-num', numbers[i]);
+			imageCrystal.attr('src', crystals[i]);
+			imageCrystal.attr('alt', 'crystals');
+			imageCrystal.addClass('crystalImage')
+			$('#crystals').append(imageCrystal);
+		}
+	}
+
+	function newGame() {
+
+		counter = 0;
+		$('#yourScore').text(counter);
+
+		function randomIntFromInterval(min,max){
+		   	return Math.floor(Math.random()*(max-min+1)+min);
+			}
+
+		var numberToGuess = randomIntFromInterval(19,120);
+
+		$('.value').text(numberToGuess);
+        audiostart.play();
+
+		$('.crystalImage').on('click', function(){
+		    counter = counter + parseInt($(this).data('num'));
+		   
+		    $('#yourScore').text(counter);
+
+		    if (counter == numberToGuess){
+		      $('#status').text('You won!!!!');
+		      wins ++;
+		      audioWin.play();
+		      $('#win').text(wins);
+		      console.log(wins)
+		      $('#crystals').empty();
+		      newCrystals();
+		      newGame();
+		        
+		    } else if ( counter > numberToGuess){
+		        $('#status').text('You lost!')
+		        losses ++;
+		        audioLost.play();
+		        $('#loss').text(losses);
+		        console.log(losses)
+		        $('#crystals').empty();
+		        newCrystals();
+		        newGame();
+		    }
+		});
+	}
+
+});
